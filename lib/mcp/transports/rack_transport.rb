5be2acc2fcba1468d9f4fd244db57274c13b53b8
Kevin Kelly <kkelly@spotify.com>
2025-05-20 18:20:36 -0400

    move context

--- lib/mcp/transports/rack_transport.rb
+++ lib/mcp/transports/rack_transport.rb
@@ -502,8 +502,7 @@ module FastMcp
       def process_json_request(request)
         # Parse the request body
         body = request.body.read
-        context = request['context']
-        response = process_message(body, context) || ''
+        response = process_message(body, @context || {}) || ''
         @logger.info("Response: #{response}")
 
         [200, { 'Content-Type' => 'application/json' }, response]
